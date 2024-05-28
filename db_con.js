8e5113382bf8dba689715cfe8508679b6c13f964
Kritika <2105ksharma@gmail.com>
2024-05-28 14:36:32 +0530

    database

--- db_con.js
+++ db_con.js
@@ -17,12 +17,28 @@ require('dotenv').config();
 const mysql = require('mysql');
 
 // Create a MySQL connection pool using environment variables
+// const pool = mysql.createPool({
+//     port: process.env.DB_PORT || 3306,
+//     host: process.env.DB_HOST || 'localhost',
+//     user: process.env.DB_USERNAME || 'root',
+//     password: process.env.DB_PASSWORD || 'password',
+//     database: process.env.DB_DATABASE || 'IntegratedProject',
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0,
+// }).on("error", (err) => {
+//     console.log("Failed to connect to Database - ", err);
+// });
+
+// module.exports = pool;
+
+
 const pool = mysql.createPool({
     port: process.env.DB_PORT || 3306,
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || 'password',
-    database: process.env.DB_DATABASE || 'IntegratedProject',
+    host:  'localhost',
+    user:'root',
+    password: 'password',
+    database: 'IntegratedProject',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
